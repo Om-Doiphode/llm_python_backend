f1bded0ff1d63dec74d468838ed362fd7008998c
Om Doiphode <omdoiphode161@gmail.com>
2025-03-23 19:20:31 +0530

    Added support for different models

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
-
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -15,18 +15,32 @@ load_dotenv()
 groq_api_key = os.getenv('GROQ_API_KEY')
 memory = ConversationBufferMemory()
 
+@app.route("/models", methods=['GET'])
+def get_models():
+    url = "https://api.groq.com/openai/v1/models"
+    headers = {"Authorization": f"Bearer {groq_api_key}"}
+
+    response = requests.get(url, headers=headers)
+    data = response.json()['data']
+    
+    models = []
+    for model in data:
+        if 'whisper' not in model['id'] and 'vision' not in model['id']:
+            models.append(model['id'])
+
+    if response.status_code == 200:
+        return jsonify({"models": models })
+    else:
+        return jsonify({"error": "Something went wrong"})
+
 @app.route('/chat', methods=['POST'])
 def run():
-    query = request.json.get('query', "")
-    data = {
-    "model": "llama3-70b-8192",
-    "messages": [
-        {"role": "user", "content": f"Answer the following query in markdown format': {query}"}
-        ]
-    }
+    data = request.get_json()
+    query = data.get("query","")
+    model = data.get("model","")
     
     # Make API request
-    llm = ChatGroq(groq_api_key=groq_api_key, model_name="llama3-70b-8192")
+    llm = ChatGroq(groq_api_key=groq_api_key, model_name=model)
     conversation = ConversationChain(
     llm=llm,
     verbose=True,
